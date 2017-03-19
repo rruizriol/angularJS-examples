25b25090fd9e118aee995cd62eb715f6f12dafd7
Rembrandt <rembrandtruizriol@hotmail.com>
2017-03-19 09:00:48 -0400

    Module 5 Solution

---
+++ module5_solution/src/public/public.routes.js
@@ -0,0 +1,74 @@
+(function() {
+'use strict';
+
+angular.module('public')
+.config(routeConfig);
+
+/**
+ * Configures the routes and views
+ */
+routeConfig.$inject = ['$stateProvider'];
+function routeConfig ($stateProvider) {
+  // Routes
+  $stateProvider
+    .state('public', {
+      absract: true,
+      templateUrl: 'src/public/public.html'
+    })
+    .state('public.home', {
+      url: '/',
+      templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.myinfo', {
+      url: '/myinfo',
+      templateUrl: 'src/public/myinfo/myinfo.html',
+      controller: 'MyInfoController',
+      controllerAs: 'myInfoCtrl',
+      resolve: {
+        userInfo: ['UserService', function (UserService) {
+          return UserService.getUserInfo();
+        }],
+        menuItem: ['MenuService','userInfo', function (MenuService, userInfo) {
+          if(userInfo.menuItemShortName) {
+            return MenuService.getMenuItemByShortName(userInfo.menuItemShortName);
+          }else {
+            return {};
+          }
+        }]
+      }
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/signup/signup.html',
+      controller: 'SignupController',
+      controllerAs: 'signupCtrl',
+      resolve: {
+        userInfo: ['UserService', function (UserService) {
+          return UserService.buildUserInfo();
+        }]
+      }
+    })
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/public/menu/menu.html',
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }
+    })
+    .state('public.menuitems', {
+      url: '/menu/{category}',
+      templateUrl: 'src/public/menu-items/menu-items.html',
+      controller: 'MenuItemsController',
+      controllerAs: 'menuItemsCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
+    });
+}
+})();
