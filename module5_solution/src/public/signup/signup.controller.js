656f75248184e562f7f925e924fbc93ee6f7240d
Rembrandt <rembrandtruizriol@hotmail.com>
2017-03-19 23:39:49 -0400

    Module 5 solution

--- module5_solution/src/public/signup/signup.controller.js
+++ module5_solution/src/public/signup/signup.controller.js
@@ -4,13 +4,26 @@
 angular.module('public')
 .controller('SignupController', SignupController);
 
-SignupController.$inject = ['userInfo','UserService'];
-function SignupController(userInfo, UserService) {
+SignupController.$inject = ['userInfo','UserService','MenuService'];
+function SignupController(userInfo, UserService, MenuService) {
   var $controller = this;
   $controller.userInfo = userInfo;
+  $controller.favoritedishNotFound = false;
+  $controller.savedInformation = false;
 
   $controller.signup = function() {
-    UserService.signUp($controller.userInfo);
+    var validator = MenuService.getMenuItemByShortName($controller.userInfo.menuItemShortName);
+    validator.then(function(response){
+
+      $controller.favoritedishNotFound = false;
+      $controller.savedInformation = true;
+
+      UserService.signUp($controller.userInfo);
+    }).catch(function(error){
+      $controller.favoritedishNotFound = true;
+      $controller.savedInformation = false;
+    });
+
   };
 }
 
